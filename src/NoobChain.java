b704d088fdf23ef4b10b392c44e4c16b03a5bb5e
George Liatsos <liatsos@gmail.com>
2019-04-27 00:51:17 +0300

    Store blocks in an ArrayList

--- src/NoobChain.java
+++ src/NoobChain.java
@@ -1,14 +1,19 @@
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 public class NoobChain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
     public static void main(String[] args) {
 
-        Block genesisBlock = new Block("Hi im the first block", "0");
-        System.out.println("Hash of block 1 : " + genesisBlock.hash );
+        blockchain.add(new Block("Hi im the first block", "0"));
+        blockchain.add(new Block("Hi im the second block", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("Hi im the third block", blockchain.get(blockchain.size()-1).hash));
 
-        Block secondBlock = new Block("Hi im the second block", genesisBlock.hash);
-        System.out.println("Hash of block 2 : " + secondBlock.hash );
-
-        Block thirdBlock = new Block("Hi im the third block", secondBlock.hash);
-        System.out.println("Hash of block 3 : " + thirdBlock.hash );
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
 
     }
 }
